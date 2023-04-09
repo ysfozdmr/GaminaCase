4ae2d8f0cc33f05f966e8f8fd764ca210f6b7566
yusufozdemir00 <ozdemiryusuf430@gmail.com>
2023-04-09 19:04:04 +0300

    Final

--- Assets/FenrirTemplate/Managers/AdManager.cs
+++ Assets/FenrirTemplate/Managers/AdManager.cs
@@ -1,62 +1,92 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using GoogleMobileAds.Api;
+using UnityEngine;
+using System;
+
 
 namespace Fenrir.Managers
 {
     public class AdManager : MonoBehaviour
     {
+        private InterstitialAd _interstitialAd;
+        private AdRequest adRequest;
+        public string appID;
 
-        public string iosInterstitialID;
-        public string androidInterstititialID;
-
-        private InterstitialAd interstitial;
-
-        void Start()
+        private void Start()
         {
-            DontDestroyOnLoad(this.transform);
-
-            MobileAds.Initialize(initStatus => { print("admob inited"); });
-
-            StartCoroutine(DelayedLoad());
+            MobileAds.Initialize((InitializationStatus initStatus) =>
+            {
+                LoadInterstitialAd();
+            });
         }
 
-        private void RequestInterstitial()
+
+        public void LoadInterstitialAd()
         {
-#if UNITY_ANDROID
-            string adUnitId = androidInterstititialID;
-#elif UNITY_IPHONE
-        string adUnitId = iosInterstitialID;
+            // Clean up the old ad before loading a new one.
+            if (_interstitialAd != null)
+            {
+                _interstitialAd.Destroy();
+                _interstitialAd = null;
+            }
 
-#endif
+            Debug.Log("Loading the interstitial ad.");
 
-            // Initialize an InterstitialAd.
-            this.interstitial = new InterstitialAd(adUnitId);
-            // Create an empty ad request.
-            AdRequest request = new AdRequest.Builder().Build();
-            // Load the interstitial with the request.
-            this.interstitial.LoadAd(request);
+            // create our request used to load the ad.
+            var adRequest = new AdRequest.Builder()
+                .AddKeyword("unity-admob-sample")
+                .Build();
+
+            // send the request to load the ad.
+            InterstitialAd.Load(appID, adRequest,
+                (InterstitialAd ad, LoadAdError error) =>
+                {
+                    // if error is not null, the load request failed.
+                    if (error != null || ad == null)
+                    {
+                        Debug.LogError("interstitial ad failed to load an ad " +
+                                       "with error : " + error);
+                        return;
+                    }
+
+                    Debug.Log("Interstitial ad loaded with response : "
+                              + ad.GetResponseInfo());
+
+                    _interstitialAd = ad;
+                });
         }
-
-        IEnumerator DelayedLoad()
+        private void RegisterReloadHandler(InterstitialAd ad)
         {
-            yield return new WaitForSeconds(0.5f);
-            RequestInterstitial();
-        }
+            // Raised when the ad closed full screen content.
+            ad.OnAdFullScreenContentClosed += ()=>
+            {
+                Debug.Log("Interstitial Ad full screen content closed.");
 
+                // Reload the ad so that we can show another as soon as possible.
+                LoadInterstitialAd();
+            };
+            // Raised when the ad failed to open full screen content.
+            ad.OnAdFullScreenContentFailed += (AdError error) =>
+            {
+                Debug.LogError("Interstitial ad failed to open full screen content " +
+                               "with error : " + error);
+
+                // Reload the ad so that we can show another as soon as possible.
+                LoadInterstitialAd();
+            };
+        }
         public void ShowInterstitial()
         {
-            if (this.interstitial.IsLoaded())
+            if (_interstitialAd != null && _interstitialAd.CanShowAd())
             {
-                this.interstitial.Show();
-                RequestInterstitial();
+                Debug.Log("Showing interstitial ad.");
+                _interstitialAd.Show();
+            }
+            else
+            {
+                Debug.LogError("Interstitial ad is not ready yet.");
             }
         }
-
-
-
-
+       
     }
-
 }
